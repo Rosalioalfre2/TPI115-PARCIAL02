9e42671425bfce688a91038906a2261f1373d2a4
Rosalioalfre2 <rosalioalfre2@gmail.com>
2021-11-27 22:34:05 -0600

    Modificando el JS

--- GRUPO06PARCIAL2TPI/appGrupo06/codigo.js
+++ GRUPO06PARCIAL2TPI/appGrupo06/codigo.js
@@ -5,21 +5,21 @@ var productos = null;
 function codigoCat(catstr) {
 	var code = "null";
 	switch (catstr) {
-		case "electronicos": code = "c1"; break;
-		case "joyeria": code = "c2"; break;
-		case "caballeros": code = "c3"; break;
-		case "damas": code = "c4"; break;
+		case "Deportes de motor": code = "c1"; break;
+		case "Deportes de pelota": code = "c2"; break;
+		case "Deportes de tiro": code = "c3"; break;
+		case "Deportes extremos": code = "c4"; break;
 	}
 	return code;
 }
 var orden = 0;
 
 
-function listarProductos(productos) {
-	var precio = document.getElementById("price");
-	precio.setAttribute("onclick", "orden*=-1;listarProductos(productos);");
+function listarDeportes(deportes) {
+	/* var precio = document.getElementById("price");
+	precio.setAttribute("onclick", "orden*=-1;listarProductos(productos);"); */
 
-	var num = productos.length;
+	var num = deportes.length;
 	var listado = document.getElementById("listado");
 	var formagregar = document.getElementById("formagregar");
 	var ids, titles, prices, descriptions, categories, fotos;
@@ -38,46 +38,46 @@ function listarProductos(productos) {
 	
 	if (orden === 0) { orden = -1; precio.innerHTML = "Precio" }
 	else
-		if (orden == 1) { ordenarAsc(productos, "price"); precio.innerHTML = "Precio A"; precio.style.color = "#D8E9A8" }
+		if (orden == 1) { ordenarAsc(deportes, "price"); precio.innerHTML = "Precio A"; precio.style.color = "#D8E9A8" }
 		else
-			if (orden == -1) { ordenarDesc(productos, "price"); precio.innerHTML = "Precio D"; precio.style.color = "#A7D0CD" }
+			if (orden == -1) { ordenarDesc(deportes, "price"); precio.innerHTML = "Precio D"; precio.style.color = "#A7D0CD" }
 
 	formagregar.style.display = "block";		
 	listado.style.display = "block";
 	for (nfila = 0; nfila < num; nfila++) {
-		ids[nfila].innerHTML = productos[nfila].id;
-		titles[nfila].innerHTML = productos[nfila].title;
-		descriptions[nfila].innerHTML = productos[nfila].description;
-		categories[nfila].innerHTML = productos[nfila].category;
-		catcode = codigoCat(productos[nfila].category);
+		ids[nfila].innerHTML = deportes[nfila].id;
+		titles[nfila].innerHTML = deportes[nfila].title;
+		descriptions[nfila].innerHTML = deportes[nfila].description;
+		categories[nfila].innerHTML = deportes[nfila].category;
+		catcode = codigoCat(deportes[nfila].category);
 		tr = categories[nfila].parentElement;
 		tr.setAttribute("class", catcode);
-		prices[nfila].innerHTML = "$" + productos[nfila].price;
-		fotos[nfila].innerHTML = "<img src='" + productos[nfila].image + "'>";
-		fotos[nfila].firstChild.setAttribute("onclick", "window.open('" + productos[nfila].image + "');");
+		prices[nfila].innerHTML = "$" + deportes[nfila].price;
+		fotos[nfila].innerHTML = "<img src='" + deportes[nfila].image + "'>";
+		fotos[nfila].firstChild.setAttribute("onclick", "window.open('" + deportes[nfila].image + "');");
 		Accion[nfila].innerHTML= "<button>Eliminar</button>"
-		Accion[nfila].firstChild.setAttribute("onclick","eliminarProductos('"+productos[nfila].id+"');");
+		Accion[nfila].firstChild.setAttribute("onclick","eliminarDeportes('"+deportes[nfila].id+"');");
 	}
 
 }
 
-function obtenerProductos() {
-	 fetch('https://retoolapi.dev/t9lCjr/productos')
+function obtenerDeportes() {
+	 fetch('')
 		.then(res => res.json())
 		.then(data => {
-			productos = data;
-			productos.forEach(
-				function(producto){
-					producto.price=parseFloat(producto.price)
+			deportes = data;
+			deportes.forEach(
+				function(deportes){
+					deportes.price=parseFloat(deportes.price)
 				});
-				listarProductos(data)});
+				listarDeportes(data)});
 }
 
 function agregarProductos() {
 
 	var imagenUrl = document.getElementById("AgregarUrl").value;
 	var precioTxt = document.getElementById("AgregarPrecio").value;
-	var titutoTxt = document.getElementById("AgregarTitulo").value;
+	var titutoTxt = document.getElementById("AgregarNombre").value;
 	var categoriaTxt = document.getElementById("AgregarCategoria").value;
 	var descripcionTxt = document.getElementById("AgregarDescripcion").value;
 
@@ -117,7 +117,7 @@ function eliminarProductos(iden) {
 		alert("Se ha eliminado el producto N° "+iden); 
 }
 
-function ordenarDesc(p_array_json, p_key) {
+/* function ordenarDesc(p_array_json, p_key) {
 	p_array_json.sort(function (a, b) {
 		if (a[p_key] > b[p_key]) return -1;
 		if (a[p_key] < b[p_key]) return 1;
@@ -131,4 +131,4 @@ function ordenarAsc(p_array_json, p_key) {
 		if (a[p_key] < b[p_key]) return -1;
 		return 0;
 	});
-}
+} */
