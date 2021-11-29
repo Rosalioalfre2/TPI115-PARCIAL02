605ce1f49a73fa9b94a0e2fa0c358628c3d295ed
abenabi <89744028+abenabi@users.noreply.github.com>
2021-11-29 07:34:54 -0600

    js

--- GRUPO06PARCIAL2TPI/appGrupo06/codigo.js
+++ GRUPO06PARCIAL2TPI/appGrupo06/codigo.js
@@ -1,6 +1,7 @@
-var fila="<tr><td class='id'></td><td class='Nombre'></td><td class='Descripcion'></td><td class='price'></td> <td class='imagen'></td><td class='Video'></td><td class='Categoria'></td><td class='Opciones'></td></tr>";
-	 var deportes=null;
-  function codigoCat(catstr) {
+var fila="<tr><td class='id'></td><td class='nombre'></td><td class='descripcion'></td><td class='imagen'></td> <td class='modoDeJuego'></td><td class='Video'></td><td class='Categoria'></td><td class='Opciones'></td></tr>";
+var deportes=null;
+
+function codigoCat(catstr) {
 	var code="null";
 	switch(catstr) {
 		case "Deportes de motor": code = "c1"; break;
@@ -16,7 +17,7 @@ function listarDeportes(deportes) {
 	 // var precio=document.getElementById("price"); 
 	 var num=deportes.length;
 	  var listado=document.getElementById("listado");
-	  var nombre,descripcion,imagen,modo_de_juego,lugar_de_juego,cantidad_de_jugadores,herramientas_necesarias,video,nombre_de_categoria;
+	  var nombre,descripcion,imagen,modoDeJuego,terrenoDeJuego,cantidadDeJugadores,herramientasNecesarias,video,nombreDeCategoria;
 	  var tbody=document.getElementById("tbody"),nfila=0;
 	  tbody.innerHTML="";
 	  var catcode;
@@ -76,25 +77,24 @@ function obtenerDeportes() {
 	fetch('http://localhost:3000/deportes')
 	.then(res=>res.json())
 	.then(data=>{
-		productos=data;
-		productos.forEach(
-			function(deportes){
-				deportes.cantidad_de_jugadores=parseFloat(cantidad_de_jugadores.Precio)
-			}
-		);
-		listarProductos(data)
+		deportes=data;
+		deportes.forEach(
+			function(deporte){
+				//deporte.cantidad_de_jugadores=parseFloat(cantidad_de_jugadores.Precio)
+				deporte.cantidad_de_jugadores=parseInt(producto.cantidad_de_jugadores)
+			});
+		listarDeportes(data)
 	})
-            
-
 }
 
 
 window.addEventListener("load", function(){
 	document.getElementById("nombre").focus();
 });
-function agregar(){
+
+function agregarDeportes(){
 	
-	nombre=document.getElementsByClassName("nombre").value.toString().trim();
+	/*nombre=document.getElementsByClassName("nombre").value.toString().trim();
 	descripcion=document.getElementsByClassName("descripcion").value.toString().trim(); 
 	imagen=document.getElementsByClassName("imagen").value.toString().trim();  
 	modo_de_juego=document.getElementsByClassName("modo_de_juego").value.toString().trim(); 
@@ -102,63 +102,67 @@ function agregar(){
 	cantidad_de_jugadores=document.getElementsByClassName("cantidad_de_jugadores").value.toString().trim(); 
 	herramientas_necesarias=document.getElementsByClassName("herramientas_necesarias").value.toString().trim(); 
 	video=document.getElementsByClassName("video").value.toString().trim();
-	nombre_de_categoria=document.getElementsByClassName("nombre_de_categoria").value.toString().trim(); 
+	nombre_de_categoria=document.getElementsByClassName("nombre_de_categoria").value.toString().trim();*/
+	
+	var nombreTxt=document.getElementById("AddNombre").value;
+	var descripcionTxt=document.getElementById("AddDescripcion").value;
+	var imagenUrl=document.getElementById("AddImagenUrl").value;
+	var modoDeJuegoTxt=document.getElementById("AddModoJuego").value;
+	var terrenoDeJuegoTxt=document.getElementById("AddTerrenoJuego").value;
+	var cantidadDeJugadoresTxt=document.getElementById("AddCantidadJugadores").value;
+	var herramientasNecesariasTxt=document.getElementById("AddHerramientasNecesarias").value;
+	var videoUrl=document.getElementById("AddVideoUrl").value;
+	var nombreDeCategoriaTxt=document.getElementById("AddCategoria").value;
 	
 	
-	
-	
-	
-	if(nombre==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('nombre').value="";
-		document.getElementById("nombre").focus();
-	}else if(descripcion==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('descripcion').value="";
-		document.getElementById("descripcion").focus();
-	}else if(imagen==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('imagen').value="";
-		document.getElementById("imagen").focus();
-	}else if(lugar_de_juego==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('lugar_de_juego').value="";
-		document.getElementById("lugar_de_juego").focus();
-	}else if(cantidad_de_jugadores==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('catidad_de_jugadores').value="";
-		document.getElementById("cantidad_de_jugaores").focus();
-
-	}else if(herramientas_necesarias==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('herramientas_necesarias').value="";
-		document.getElementById("herramientas_necesarias").focus();
-
-
-	}else if(video==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('video').value="";
-		document.getElementById("video").focus();
-	}else if(modo_de_juego==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('modo_de_juego').value="";
-		document.getElementById("modo_de_juego").focus();
-	}else if(nombre_de_categoria==""){
-		alert("No se permiten campos vacíos");
-		document.getElementById('nombre_de_categoria').value="";
-		document.getElementById("nombre_de_categoria").focus();
+	if(nombreTxt==""){
+		alert("No se permiten campos vacíos. Escriba un nombre.");
+		document.getElementById("AddNombre").value="";
+		document.getElementById("AddNombre").focus();
+	}else if(descripcionTxt==""){
+		alert("No se permiten campos vacíos. Agregué una descripción.");
+		document.getElementById("AddDescripcion").value="";
+		document.getElementById("AddDescripcion").focus();
+	}else if(imagenUrl==""){
+		alert("No se permiten campos vacíos. Agregue una dirección de imagen.");
+		document.getElementById("AddImagenUrl").value="";
+		document.getElementById("AddImagenUrl").focus();
+	}else if(modoDeJuegoTxt==""){
+		alert("No se permiten campos vacíos. Seleccione un modo de juego.");
+		document.getElementById("AddModoJuego").value="";
+		document.getElementById("AddModoJuego").focus();
+	}else if(terrenoDeJuegoTxt==""){
+		alert("No se permiten campos vacíos. Seleccione un terreno de juego.");
+		document.getElementById("AddTerrenoJuego").value="";
+		document.getElementById("AddTerrenoJuego").focus();
+	}else if(herramientasNecesariasTxt==""){
+		alert("No se permiten campos vacíos. Agregué las herramientas necesarias.");
+		document.getElementById("AddHerramientasNecesarias").value="";
+		document.getElementById("AddHerramientasNecesarias").focus();
+	}else if(videoUrl==""){
+		alert("No se permiten campos vacíos. Agregue una dirección de video.");
+		document.getElementById("AddVideoUrl").value="";
+		document.getElementById("AddVideoUrl").focus();
+	}else if(cantidadDeJugadoresTxt==""){
+		alert("No se permiten campos vacíos. Agregue una cantidad de jugadores.");
+		document.getElementById("AddCantidadJugadores").value="";
+		document.getElementById("AddCantidadJugadores").focus();
+	}else if(nombreDeCategoriaTxt==""){
+		alert("No se permiten campos vacíos. Seleccione una categoria.");
+		document.getElementById("AddCategoria").value="";
+		document.getElementById("AddCategoria").focus();
 	}else{
-		if(document.getElementById('precio').value>=0){
-			var producto={
-				Nombre:nombre,
-				Descripcion:descripcion,
-				imagen:imagen,
-				modo_de_juego:modo_de_juego,
-				lugar_de_juego:lugar_de_juego,
-				cantidad_de_jugadores:cantidad_de_jugadores,
-				herramientas_necesarias:herramientas_necesarias,
-				Video:video,
-				nombre_de_categoria:nombre_de_categoria
+		if(document.getElementById("AddCantidadJugadores").value>=0){
+			var deporte={
+				nombre:nombreTxt,
+				descripcion:descripcionTxt,
+				imagen:imagenUrl,
+				modo_de_juego:modoDeJuegoTxt,
+				terreno_de_juego:terrenoDeJuegoTxt,
+				cantidad_de_jugadores:cantidadDeJugadoresTxt,
+				herramientas_necesarias:herramientasNecesariasTxt,
+				video:videoUrl,
+				nombre_de_categoria:nombreDeCategoriaTxt
 			}
 			fetch('http://localhost:3000/deportes',
 			{ method:"POST",
@@ -168,8 +172,10 @@ function agregar(){
 					'Content-type': 'application/json; charset=UTF-8',	   
 				 }
 			})
-			.then(res=>res.json())
-			.then(data=>data);
+			.then(response=>response.json())
+			.then(data=>{deportes = data; obtenerDeportes()});
+			alert("Se agregó el deporte: "+nombreTxt);
+			/*
 			document.getElementById('nombre').value="";
 			document.getElementById('descripcion').value="";
 			document.getElementById('imagen').value="";
@@ -179,13 +185,9 @@ function agregar(){
 			document.getElementById('herramientas_necesesarias').value="";
 			document.getElementById('video').value="";
 			document.getElementById('nombre_de_categoria').value="";
-
-
 			orden*=0;
-			alert("Se agregó el deporte: "+nombre);
 			window. location. reload();
-		}else{
-			alert("Debe ingresar los datos de manera correcta");
+			*/
 		}
 	}
 }
@@ -212,7 +214,7 @@ function eliminar(id) {
 			obtenerDeportes();
 			alert("Se ha eliminado el deporte N° " + id);
 }
-
+/*
 function ordenarDesc(p_array_json, p_key) {
 	p_array_json.sort(function (a, b) {
 	   if(a[p_key] > b[p_key]) return -1;
@@ -228,3 +230,4 @@ function ordenarDesc(p_array_json, p_key) {
  return 0;
 	});
  }
+ */
